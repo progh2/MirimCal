85c6c8f2f999cb57630095ac77a1c90e44b3abd4
Gihun Ham <ham@e-mirim.hs.kr>
2020-06-18 14:45:14 +0900

    초기 커밋.

---
+++ MirimCal/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MirimCal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var school = ConfigurationManager.AppSettings["school"];
+            var version = ConfigurationManager.AppSettings["version"];
+            this.Text = school + " 신입생 내신성적 산출 프로그램 [ " + version + " ]";
+        }
+    }
+}
